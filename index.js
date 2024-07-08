23169ccaa4163cd4ca8b0859610b42c4a2aece8e
Coqui31 <160015444+Coqui31@users.noreply.github.com>
2024-07-08 10:54:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import getProductos from "./js/getProductos.js";
+import { botonEnviarFormulario,  botonEnviarFormularioClick } from "./js/botonEnviar.js";
+import botonLimpiar from "./js/botonLimpiar.js";
+
+// Implementación de la funcionalidad de cargar los productos desde la api
+getProductos;
+
+// Implementación de la funcionalidad del formulario: botonLimpiar
+botonLimpiar;
+
+// Implementación de la funcionalidad del formulario: botonEnviar
+botonEnviarFormulario;
+botonEnviarFormularioClick;
